3c5234fcc8882d950604fa3816ee5f9674268821
KevohMras <kevohyutman21@gmail.com>
2024-12-21 11:55:47 +0300

    whitenoise addition

--- harvest/settings.py
+++ harvest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3d^&+x@c604^6kgwl30($056^yzo0vgxui4^j$+pzjk8vgg27e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
